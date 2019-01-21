31dd1e83ad8e43a42f6a4c8d6697cdb646516b6e
Case Van Arkel <casevanarkel@gmail.com>
2019-01-21 10:25:01 -0800

    setting up portfolio app

--- public/pages/portfolio.js
+++ public/pages/portfolio.js
@@ -10,11 +10,18 @@ angular.module('portfolioApp', ['ngAnimate'])
 
   this.initializeApp = function(){
     
+      alert('made it in initializeApp');
+
       //After the window has loaded completely
       window.addEventListener("load", function(){
 
+        alert('made it in window.load');
+
           //Set the app state to 'about'
           $scope.$apply(function(){ 
+
+            alert('made it in $scope.apply');
+
               portfolioCtrl.setAppState('about'); 
           });
 
@@ -30,10 +37,12 @@ angular.module('portfolioApp', ['ngAnimate'])
     return this.appState;
   }
 
+  alert('made it here!');
+
   //Run on page load
   this.initializeApp();
 
-})
+});
 
 .directive('portfolioDirective', function() {
   return {
